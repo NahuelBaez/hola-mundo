a6bd3a720d6fd5375a7f36c1bc6ef43063abafb9
Matías Nahuel Báez <38184104+NahuelBaez@users.noreply.github.com>
2018-06-22 19:34:24 -0300

    Update HolaMundo.java

--- src/main/java/loom/hola_mundo/HolaMundo.java
+++ src/main/java/loom/hola_mundo/HolaMundo.java
@@ -1,8 +1,19 @@
 package loom.hola_mundo;
 
+/**
+ * @author Nahuel
+ * @version 22/06/2018
+ * 
+ */
 public class HolaMundo {
 
+    /**
+	 * Este método devuelve un String con un saludo a la persona que recibe por parámetro.
+	 *  
+	 * @param persona
+	 * @return saludo
+	 */
     public String saludar(final String persona) {
-        throw new RuntimeException("¡Implementar este método!");
+        return "¡Hola, " + persona + "!";
     }
 }
